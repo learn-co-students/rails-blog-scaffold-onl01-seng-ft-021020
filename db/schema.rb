bb996c434e76ccb3cdf76ed8c6491b95472ff485
rfgr87 <royguzrod@gmail.com>
2020-04-17 18:56:39 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_17_225510) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.float "price"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
